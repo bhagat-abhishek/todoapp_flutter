90d3f03c68fbc6348133e963a42707cb6ab19fbb
bhagat-abhishek <inst.abhishek@gmail.com>
2024-11-24 23:33:35 +0530

    error quickfix

--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -25,7 +25,7 @@ class TodoList extends StatelessWidget {
       ),
       child: Slidable(
         endActionPane: ActionPane(
-          motion: StretchMotion(),
+          motion: const StretchMotion(),
           children: [
             SlidableAction(
               onPressed: deleteFunction,
